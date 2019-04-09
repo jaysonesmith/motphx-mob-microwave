2e1684a629d5877ed764c52ebc6fbb73fc63cc56
Nelson Chen <crazysim@gmail.com>
2019-04-08 19:31:57 -0700

    Mob 2019

--- test/test_spec.js
+++ test/test_spec.js
@@ -1,15 +1,28 @@
-const {describe, it} = require('mocha');
+const { describe, it } = require('mocha');
 const expect = require('chai').expect;
-const microwave = require('../src/microwave');
+const { microwave } = require('../src/microwave');
 
-describe("microwave", function() {
-    describe("when start is pressed and no time is specified", function() {
-        it("beeps", function() {
+describe("microwave", function () {
+    describe("when start is pressed and no time is specified", function () {
+        it("beeps", function () {
             // arrange
             const expected = "beep!"
 
             // act
-            var actual = '';
+            var actual = microwave();
+
+            // assert
+            expect(actual).to.equal(expected);
+        })
+    })
+    describe("when start is pressed and 5 seconds is specified", function () {
+        it("beeps", function () {
+            // arrange
+            const input = 5;
+            const expected = 5
+
+            // act
+            var actual = microwave(input);
 
             // assert
             expect(actual).to.equal(expected);
