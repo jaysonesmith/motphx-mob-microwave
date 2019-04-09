823a93b6eb442891d288fddb45c2eed949c2ea33
Jayson Smith <github@nes.33mail.com>
2019-04-08 19:10:36 -0700

    JS commit

---
+++ tests/test_spec.js
@@ -0,0 +1,15 @@
+
+describe("microwave", function() {
+    describe("when start is pressed and no time is specified", function() {
+        it("beeps", function() {
+            // arrange
+            const expected = "beep!"
+
+            // act
+            
+
+            // assert
+            expect(actual).to.be(expected)
+        })
+    })
+})
